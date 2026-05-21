42c300d5485d6309db80d2472bbf63fab034b1a6
andrewkeig <andrew.keig@gmail.com>
2026-05-21 10:46:17 +0100

    update versions

--- src/services/starwars.ts
+++ src/services/starwars.ts
@@ -7,6 +7,13 @@ export type StarWarsResponseType = {
 }
 
 export const search = async (query: string): Promise<StarWarsResponseType> => {
-  const response = await fetch(`https://swapi.dev/api/people/?search=${query}`)
-  return response.json()
+  const response = await fetch(`https://swapi.info/api/people`)
+  const data = await response.json()
+  const results = data.filter((c: CharacterType) => c.name?.toLowerCase().includes(query.toLowerCase()))
+
+  return {
+    results,
+    count: results.length,
+    query
+  }
 }
